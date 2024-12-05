76955ebf8a75152d7c6cdfbb16ad637e46d1e87f
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-05 17:38:48 +0600

    added some components, and assets, and providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
+import AuthProvider from './providers/AuthProvider';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: 'signin',
+        element: <SignIn></SignIn>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
